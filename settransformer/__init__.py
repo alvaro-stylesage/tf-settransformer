10ec6e6dd7ad46ee199aefd01a229a03f58fe01e
David Ludwig <davidludwigii@gmail.com>
2022-07-05 21:16:30 +0000

    Forward mask in MAB

--- settransformer/__init__.py
+++ settransformer/__init__.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
+import tensorflow.keras.backend as K
 
 # Utility Functions --------------------------------------------------------------------------------
 
@@ -36,17 +37,75 @@ def custom_layers():
 
 # Layer Utility Functions --------------------------------------------------------------------------
 
-def spectral_norm(*args, **kwargs):
-    import tensorflow_addons as tfa
-    return tfa.layers.SpectralNormalization(*args, **kwargs)
+# def spectral_norm(*args, **kwargs):
+    # import tensorflow_addons as tfa
+    # return tfa.layers.SpectralNormalization(*args, **kwargs)
+    # return DenseSN(*args, **kwargs)
 
 def spectral_dense(dim, use_spectral_norm=False, activation=None, **kwargs):
-    layer = keras.layers.Dense(dim, **kwargs)
-    if use_spectral_norm and activation is None:
-        layer = spectral_norm(layer)
-    return layer
+    if not use_spectral_norm or activation is not None:
+        return keras.layers.Dense(dim, activation=activation, **kwargs)
+    return DenseSN(dim, **kwargs)
 
 # Layer Definitions --------------------------------------------------------------------------------
+
+class DenseSN(keras.layers.Dense):
+    """
+    https://github.com/IShengFang/SpectralNormalizationKeras/blob/master/SpectralNormalizationKeras.py#L17-L69
+    Spectral Normalization Layer
+    """
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1]
+        self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.u = self.add_weight(shape=tuple([1, self.kernel.shape.as_list()[-1]]),
+                                 initializer=keras.initializers.RandomNormal(0, 1),
+                                 name='sn',
+                                 trainable=False)
+        self.input_spec = keras.layers.InputSpec(min_ndim=2, axes={-1: input_dim})
+        self.built = True
+        
+    def call(self, inputs, training=None):
+        def _l2normalize(v, eps=1e-12):
+            return v / (K.sum(v ** 2) ** 0.5 + eps)
+        def power_iteration(W, u):
+            _u = u
+            _v = _l2normalize(K.dot(_u, K.transpose(W)))
+            _u = _l2normalize(K.dot(_v, W))
+            return _u, _v
+        W_shape = self.kernel.shape.as_list()
+        #Flatten the Tensor
+        W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+        _u, _v = power_iteration(W_reshaped, self.u)
+        #Calculate Sigma
+        sigma=K.dot(_v, W_reshaped)
+        sigma=K.dot(sigma, K.transpose(_u))
+        #normalize it
+        W_bar = W_reshaped / sigma
+        #reshape weight tensor
+        if training in {0, False}:
+            W_bar = K.reshape(W_bar, W_shape)
+        else:
+            with tf.control_dependencies([self.u.assign(_u)]):
+                 W_bar = K.reshape(W_bar, W_shape)  
+        output = K.dot(inputs, W_bar)
+        if self.use_bias:
+            output = K.bias_add(output, self.bias, data_format='channels_last')
+        if self.activation is not None:
+            output = self.activation(output)
+        return output
 
 @CustomLayer
 class VaswaniMultiHeadAttention(keras.layers.Layer):
@@ -168,7 +227,9 @@ class MultiHeadAttentionBlock(keras.layers.Layer):
         if self.use_layernorm:
             out = self.layernorms["final"](out)
         return out
-
+    
+    def compute_mask(self, inputs, mask):
+        return mask
 
     def call(self, inputs: tuple, training=None):
         x, y = inputs[0], inputs[1]
